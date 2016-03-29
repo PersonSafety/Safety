5e0eb1651c92a78a282e462e1cf0590bdf15e2f1
lupangdelu <Huangtq@windows10.microdone.cn>
2016-03-29 18:52:52 +0800

    utils

--- src/main/java/com/cn/safety/crawler/NewsCrawler.java
+++ src/main/java/com/cn/safety/crawler/NewsCrawler.java
@@ -5,6 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.cn.safety.crawler.processor.LegaldailyProcessor;
+import com.cn.safety.crawler.processor.XinhuanetProcessor;
 import com.cn.safety.service.impl.UserServiceImpl;
 
 public class NewsCrawler {
@@ -12,9 +13,8 @@ public class NewsCrawler {
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring-mvc.xml");
-		 BeanFactory factory = (BeanFactory) applicationContext;  
-        final LegaldailyProcessor jobCrawler = (LegaldailyProcessor)factory.getBean("legaldailyProcessor");
-//        final UserServiceImpl jobCrawler = (UserServiceImpl)factory.getBean("userService");
-        jobCrawler.crawl();
+		BeanFactory factory = (BeanFactory) applicationContext;
+		final XinhuanetProcessor myCrawler = (XinhuanetProcessor) factory.getBean("xinhuanetProcessor");
+		myCrawler.crawl();
 	}
 }
